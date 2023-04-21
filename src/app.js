3bd226f15aec1a082feba4f47b35ca96b475ee6c
Anupriya-rgb <feronarockiam1493@gmail.com>
2023-04-21 14:19:59 +0530

    2

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4003;
 
 const app = express();
 const httpServer = http.createServer(app);
